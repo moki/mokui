e904b14bd1c83592799e74562308b7a36fd44eeb
Kirill Morozov (moki) <morozov.kirill.moki@gmail.com>
2019-11-18 13:52:12 +0300

    feat(mokui-header): expose secondary_action selector

--- packages/mokui-header/constants.ts
+++ packages/mokui-header/constants.ts
@@ -7,5 +7,6 @@ export const numbers = {};
 export const strings = {
         ACTION_PRIMARY_SELECTOR: ".header__action_primary",
         ACTION_PRIMARY_EVENT: "mokui-header:action_primary",
+        ACTION_SECONDARY_SELECTOR: ".header__action_secondary",
         ROOT_SELECTOR: ".header"
 };
